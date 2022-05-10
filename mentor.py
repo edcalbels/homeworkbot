674fd61de5742e1dfddeb846426e65c13bb93dc9
edcalbels <103530093+edcalbels@users.noreply.github.com>
2022-05-10 17:32:11 +0600

    Add files via upload

---
+++ mentor.py
@@ -0,0 +1,15 @@
+class Figure:
+    def __init__(self,unit:int):
+        self.unit = unit
+
+    def calculate_area(self,unit):
+        unit * unit
+
+rectangle = Figure(4)
+rectangle.calculate_area(())
+
+
+
+
+
+
