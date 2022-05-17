324446340328122f365744bc4f4d5e4e1376a095
edcalbels <103530093+edcalbels@users.noreply.github.com>
2022-05-17 17:24:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,89 +3,109 @@
 
 
 
-from aiogram import types, Bot, Dispatcher
+# from aiogram import types, Bot, Dispatcher
+# from aiogram.utils import executor
+# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+# import logging
+# from decouple import config
+
+# TOKEN = config("TOKEN")
+# bot = Bot(TOKEN)
+# dp = Dispatcher(bot=bot)
+
+
+# @dp.message_handler(commands=['start'])
+# async def start(message: types.Message):
+#     await message.answer(f"Привет мой хозяин {message.from_user.full_name}")
+
+
+# @dp.message_handler(commands=['quiz'])
+# async def quiz_1(message: types.Message):
+#     markup = InlineKeyboardMarkup()
+#     button_call_1 = InlineKeyboardButton("NEXT", callback_data="button_call_1")
+#     markup.add(button_call_1)
+
+#     question = "На каком этаже находится ГИКТЕК ?"
+#     answers = ['1', '2', '3', '4', '5', '6', '7']
+#     await bot.send_poll(
+#         chat_id=message.from_user.id,
+#         question=question,
+#         options=answers,
+#         is_anonymous=False,
+#         type='quiz',
+#         correct_option_id=3,
+#         explanation="между 3 и 5",
+#         explanation_parse_mode=ParseMode.MARKDOWN_V2,
+#         reply_markup=markup
+#     )
+
+# @dp.callback_query_handler(lambda call: call.data == "button_call_1")
+# async def quiz_2(call: types.CallbackQuery):
+#     markup = InlineKeyboardMarkup()
+#     button_call_2 = InlineKeyboardButton("NEXT", callback_data="button_call_2")
+#     markup.add(button_call_2)
+
+#     question = "Где находтся ГикТек?"
+#     answers = [
+#         'БЦ Victory',
+#         'БЦ Мегаком',
+#         'da fig ego',
+
+#     ]
+#     await bot.send_poll(
+#         chat_id=call.from_user.id,
+#         question=question,
+#         options=answers,
+#         is_anonymous=False,
+#         type='quiz',
+#         correct_option_id=0,
+#         explanation="начинается на V",
+#         explanation_parse_mode=ParseMode.MARKDOWN_V2,
+#         reply_markup=markup
+#     )
+
+# @dp.callback_query_handler(lambda call: call.data == "button_call_2")
+# async def quiz_2(call: types.CallbackQuery):
+
+
+#     photo = open("media2/mem.jpeg", "rb")
+#     await bot.send_photo(call.from_user.id, photo=photo)
+#     # await bot.send_poll(
+#     #     chat_id=call.from_user.id,
+#     #     question=question,
+#     #     options=answers,
+#     #     is_anonymous=False,
+#     #     type='quiz',
+#     #     correct_option_id=3,
+#     #     explanation="Это же легко",
+#     #     explanation_parse_mode=ParseMode.MARKDOWN_V2,
+#     # )
+
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     await bot.send_message(message.from_user.id, int(message.text)**2)
+
+
+
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.INFO)
+#     executor.start_polling(dp, skip_updates=True)
+
+
+
+
+
+
 from aiogram.utils import executor
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from config import dp
+from hendlers import admin, client, callback, extra, fsm_anketa
 import logging
-from decouple import config
 
-TOKEN = config("TOKEN")
-bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
-
-
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.answer(f"Привет мой хозяин {message.from_user.full_name}")
-
-
-@dp.message_handler(commands=['quiz'])
-async def quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-    button_call_1 = InlineKeyboardButton("NEXT", callback_data="button_call_1")
-    markup.add(button_call_1)
-
-    question = "На каком этаже находится ГИКТЕК ?"
-    answers = ['1', '2', '3', '4', '5', '6', '7']
-    await bot.send_poll(
-        chat_id=message.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=3,
-        explanation="между 3 и 5",
-        explanation_parse_mode=ParseMode.MARKDOWN_V2,
-        reply_markup=markup
-    )
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_1")
-async def quiz_2(call: types.CallbackQuery):
-    markup = InlineKeyboardMarkup()
-    button_call_2 = InlineKeyboardButton("NEXT", callback_data="button_call_2")
-    markup.add(button_call_2)
-
-    question = "Где находтся ГикТек?"
-    answers = [
-        'БЦ Victory',
-        'БЦ Мегаком',
-        'da fig ego',
-
-    ]
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=0,
-        explanation="начинается на V",
-        explanation_parse_mode=ParseMode.MARKDOWN_V2,
-        reply_markup=markup
-    )
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_2")
-async def quiz_2(call: types.CallbackQuery):
-
-
-    photo = open("media2/mem.jpeg", "rb")
-    await bot.send_photo(call.from_user.id, photo=photo)
-    # await bot.send_poll(
-    #     chat_id=call.from_user.id,
-    #     question=question,
-    #     options=answers,
-    #     is_anonymous=False,
-    #     type='quiz',
-    #     correct_option_id=3,
-    #     explanation="Это же легко",
-    #     explanation_parse_mode=ParseMode.MARKDOWN_V2,
-    # )
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.from_user.id, int(message.text)**2)
-
-
+client.register_handler_client(dp)
+admin.register_handlers_admin(dp)
+fsm_anketa.register_hendler_fsmanketa(dp)
+callback.register_handler_callback(dp)
+extra.register_hendler_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
@@ -116,20 +136,13 @@ if __name__ == "__main__":
 
 
 
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     await bot.send_message(message.from_user.id, message.text)
 
 
-
-
-
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.INFO)
+#     executor.start_polling(dp, skip_updates=True)
 
 
